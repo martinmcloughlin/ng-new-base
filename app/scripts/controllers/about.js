3f884eb33c2b05f16f9e4d19753ebd9de3014071
Martin McLoughlin <martin.mcloughlin@spindriftgroup.com>
2016-07-08 13:56:03 +0100

    using yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmTrainApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mmTrainApp
+ */
+angular.module('mmTrainApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmTrainApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mmTrainApp
+ */
+angular.module('mmTrainApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
